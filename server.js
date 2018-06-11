c3708b83efa84d78e870e2c6f70a66e793a64c15
puppitty <33668867+puppitty@users.noreply.github.com>
2018-06-11 13:50:17 -0400

    Initial file structure

---
+++ server.js
@@ -0,0 +1,104 @@
+// Using the tools and techniques you learned so far,
+// you will scrape a website of your choice, then place the data
+// in a MongoDB database. Be sure to make the database and collection
+// before running this exercise.
+
+// Consult the assignment files from earlier in class
+// if you need a refresher on Cheerio.
+
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+var bodyParser = require("body-parser");
+// Require request and cheerio. This makes the scraping possible
+var axios = require("axios");
+var request = require("request");
+var cheerio = require("cheerio");
+var fs = require('fs');
+var path = require("path")
+var mongoose = require("mongoose");
+
+// Initialize Express
+var app = express();
+
+var db = require("./models");
+var PORT = 3000;
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function (error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function (req, res) {
+  res.send("Hello world");
+});
+
+// TODO: make two more routes
+
+// Route 1
+// =======
+// This route will retrieve all of the data
+// from the scrapedData collection as a json (this will be populated
+// by the data you scrape using the next route)
+app.get("/all", function (req, res) {
+  db.scrapedData.find({}, function (error, found) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(found);
+    }
+  });
+});
+
+app.get("/scrape", function (req, res) {
+  // Make a request call to grab the HTML body from the site of your choice
+  request("http://www.nytimes.com", function (error, response, html) {
+
+    // Load the HTML into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    var $ = cheerio.load(html);
+
+    // An empty array to save the data that we'll scrape
+    var results = [];
+
+    // Select each element in the HTML body from which you want information.
+    // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+    // but be sure to visit the package's npm page to see how it works
+    $("h2.story-heading").each(function (i, element) {
+
+      var link = $(element).children().attr("href");
+      var title = $(element).children().text();
+
+      // Save these results in an object that we'll push into the results array we defined earlier
+      db.scrapedData.insert({
+        title: title,
+        link: link
+      });
+      // res.send("Scrape Complete");
+    });
+  });
+});
+
+
+
+// Route 2
+// =======
+// When you visit this route, the server will
+// scrape data from the site of your choice, and save it to
+// MongoDB.
+// TIP: Think back to how you pushed website data
+// into an empty array in the last class. How do you
+// push it into a MongoDB collection instead?
+
+/* -/-/-/-/-/-/-/-/-/-/-/-/- */
+
+// Listen on port 3000
+app.listen(3000, function () {
+  console.log("App running on port 3000!");
+});
